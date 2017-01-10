92b11a1f89342280d9e919eb5bf9778b381ec4df
Peter Holák <peterholak@gmail.com>
2017-01-10 01:23:46 +0100

    Create setup-monaco.sh

---
+++ setup-monaco.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+TYPESCRIPT_VERSION="2.1.4"
+
+git clone https://github.com/Microsoft/monaco-editor.git
+cd monaco-editor
+npm install
+cd node_modules
+rm -rf monaco-typescript
+git clone https://github.com/Microsoft/monaco-typescript.git
+cd monaco-typescript
+npm install --save-dev gulp-requirejs@1.0.0-rc1
+npm install --save-dev typescript@${TYPESCRIPT_VERSION}
+npm install
+gulp import-typescript
+gulp release
+cd ../..
+gulp release
